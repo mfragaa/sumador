8fcc41e3cb2cd601c2c2b1069d30aacd24691655
mfraga <mfraga@itmorelia.edu.mx>
2020-04-24 18:30:18 -0500

    Se incluye el calculo la suma de los elementos del arreglo

--- main.c
+++ main.c
@@ -8,11 +8,14 @@
 
 int main(int argc, char **argv)
 {
-	int n, arreglo[100],indice;
+	int n, arreglo[100],indice,suma=0;
     
     printf("Cuantos numeros se van a sumar:");
     scanf("%d",&n);
     leeArreglo( n, arreglo);
+    for(indice=0;indice<n;indice++) 
+        suma+=arreglo[indice];
+    printf("La suma es: %d \n",suma);
 	return 0;
 }
 
